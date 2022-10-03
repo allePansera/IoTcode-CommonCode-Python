9347b58d68f3f292bb4ea19988e381d65451e3a7
allePansera <allepansera2001@gmail.com>
2022-10-03 12:48:11 +0200

    Upload of edited original files

--- OOP/ControlUnit/__init__.py
+++ OOP/ControlUnit/__init__.py
@@ -1,3 +1,24 @@
 class ControlUnit:
     def __init__(self,pointList=[]):
         self.pointList = pointList
+
+    def addPoint(self,pointOfIntrest):
+        self.pointList.append(pointOfIntrest)
+
+    def deletePoint(self,pointOfIntrest):
+        """This method works under index of item values"""
+        indexToRemove = -1
+        for index, PoI in enumerate(self.pointList):
+            if PoI.name == pointOfIntrest.name:
+                indexToRemove = index
+        if indexToRemove != -1:
+            self.pointList.pop(indexToRemove)
+
+    def updatePoint(self,pointOfIntrest):
+        """This method works under index of item values"""
+        indexToEdit = -1
+        for index, PoI in enumerate(self.pointList):
+            if PoI.name == pointOfIntrest.name:
+                indexToEdit = index
+        if indexToEdit != -1:
+            self.pointList[indexToEdit]=pointOfIntrest
--- OOP/PointOfIntrest/__init__.py
+++ OOP/PointOfIntrest/__init__.py
@@ -3,3 +3,24 @@ class PointOfIntrest:
         self.name=name
         self.deviceList=deviceList
         self.geolocation=geolocation
+
+    def addDevice(self,device):
+        self.deviceList.append(device)
+
+    def removeDevice(self,device):
+        """This method works under index of item values"""
+        indexToRemove=-1
+        for index,dev in enumerate(self.deviceList):
+            if dev.id==device.id:
+                indexToRemove=index
+        if indexToRemove!=-1:
+            self.deviceList.pop(indexToRemove)
+
+    def updateDevice(self,device):
+        """This method works under index of item values"""
+        indexToEdit = -1
+        for index, dev in enumerate(self.deviceList):
+            if dev.id == device.id:
+                indexToEdit = index
+        if indexToEdit != -1:
+            self.deviceList[indexToEdit]=device
